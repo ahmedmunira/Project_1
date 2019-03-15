0808f06ae63bc264ed1568f4c3d6986c930b826e
Munira <mahmed0318@gmail.com>
2019-03-15 11:33:49 -0400

    Another line is added.

--- src/com/shift/qa/HelloClass.java
+++ src/com/shift/qa/HelloClass.java
@@ -4,5 +4,6 @@ public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(" I am adding this line");
+        System.out.println("Another line is added for testing purpose.");
     }
 }
