b67bad22c33bd07dbc9bdaf8bb39248f95515422
Munira <mahmed0318@gmail.com>
2019-03-15 11:23:04 -0400

    add new line " I am adding this line"

--- src/com/shift/qa/HelloClass.java
+++ src/com/shift/qa/HelloClass.java
@@ -3,5 +3,6 @@ package com.shift.qa;
 public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(" I am adding this line");
     }
 }
